510ac66c6199b1f090d159327b455f08ba4d1688
jithendra <jithendra@sasianet.com>
2018-02-14 10:28:41 +0530

    changed config

--- Server.js
+++ Server.js
@@ -1,15 +1,13 @@
-var http = require("http");
+var http = require('http');
 
-http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Kaema App is Running\n');
-}).listen(8081);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello From Kaema!");
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
