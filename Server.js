f83713070f407e266112551026fcea137e9cd552
jithendra <jithendra@sasianet.com>
2018-02-14 11:39:30 +0530

    added Firebase Controls

--- Server.js
+++ Server.js
@@ -1,13 +1,17 @@
 var http = require('http');
+var chkSum=require('./myModule.js');
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello From Kaema!");
+    let answer=chkSum.checkSum(145,200);
+    response.end("Hello From Kaema!"+answer);
+    
 
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
+chkSum.alwaysRunner();
 
 console.log("Server running at http://localhost:%d", port);
---
+++ myModule.js
@@ -0,0 +1,31 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./kaema-159c6-firebase-adminsdk-phdnc-9a139dbaf7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kaema-159c6.firebaseio.com"
+});
+
+exports.checkSum=function (num1,num2){
+     return num1+num2;
+}
+
+
+exports.alwaysRunner=function (){
+    firebaseData();
+   // setInterval(function(){logger()}, 5000);
+}
+
+function logger(){
+    console.log('This is being called Continously every 5 sec')
+}
+
+
+function firebaseData(){
+    var db = admin.database();
+    var ref = db.ref("/post");
+    ref.once("value", function(snapshot) {
+      console.log(snapshot.val());
+    });
+}
