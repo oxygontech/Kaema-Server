17e3cdead1696687776b0e13605b2f2bc84f674b
Jithendra <jithendrathenu@gmail.com>
2018-03-05 22:02:58 +0530

    Added update for other sections which refer the profile object

--- firebase-modules/profile-module.js
+++ firebase-modules/profile-module.js
@@ -18,7 +18,10 @@ exports.profile_changed_listener=function (){
     var ref = db.ref("/profile");
     ref.on("child_changed", function(snapshot) {
 
-      var userProfile= snapshot.val();
+      var profile= snapshot.val();
+      
+
+
       var postRef=db.ref("/post");
       postRef.orderByChild("userId").equalTo(snapshot.key).once("child_added", function(postList) {
         
@@ -26,7 +29,49 @@ exports.profile_changed_listener=function (){
 
         var updatePostRef=db.ref("/post/"+postList.key);
         updatePostRef.update({
-          userProfile
+          userProfile:profile
+        });
+      });
+
+
+
+      var requestRef=db.ref("/request");
+      requestRef.orderByChild("postedUser").equalTo(snapshot.key).once("child_added", function(requestList) {
+        
+        var updatePostRef=db.ref("request/"+requestList.key+"/post");
+        updatePostRef.update({
+          userProfile:profile
+        });
+      });
+
+
+      var sharedRef=db.ref("/shared");
+      sharedRef.orderByChild("sharedUser").equalTo(snapshot.key).once("child_added", function(sharedList) {
+        
+        var updatePostRef=db.ref("shared/"+sharedList.key+"/post");
+        updatePostRef.update({
+          userProfile:profile
+        });
+      });
+
+
+      var requestedUserRef=db.ref("/request");
+      requestedUserRef.orderByChild("requestedUser").equalTo(snapshot.key).once("child_added", function(sharedList) {
+        
+        var updatePostRef=db.ref("request/"+sharedList.key);
+        updatePostRef.update({
+          requestedUserProfile:profile
+        });
+      });
+
+
+
+      var sharedUserRef=db.ref("/shared");
+      sharedUserRef.orderByChild("receivedUser").equalTo(snapshot.key).once("child_added", function(sharedList) {
+        
+        var updatePostRef=db.ref("shared/"+sharedList.key);
+        updatePostRef.update({
+          receivedUserProfile:profile
         });
       });
       
