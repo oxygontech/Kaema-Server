6cefa0b1a01abd0d9118d5524c36fb4260a8b8b4
Jithendra <jithendrathenu@gmail.com>
2018-03-07 21:09:50 +0530

    added express framework

--- Server.js
+++ Server.js
@@ -1,7 +1,28 @@
 var http = require('http');
 var profileModule=require('./firebase-modules/profile-module.js');
 
-var server = http.createServer(function(request, response) {
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+
+
+var router = express.Router(); 
+var port = process.env.PORT || 1337;
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Server is up and running' });   
+});
+
+app.use('/service', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+profileModule.profile_changed_listener();
+
+
+
+
+/*var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello From Kaema!");
@@ -9,8 +30,10 @@ var server = http.createServer(function(request, response) {
 
 });
 
+
+
 var port = process.env.PORT || 1337;
 server.listen(port);
 profileModule.profile_changed_listener();
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at http://localhost:%d", port);*/
