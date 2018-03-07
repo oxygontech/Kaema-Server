390916cfac27fef03951039d3b1a636bdc4f2846
Jithendra <jithendrathenu@gmail.com>
2018-03-07 21:16:07 +0530

    fixed access error

--- Server.js
+++ Server.js
@@ -9,6 +9,26 @@ var bodyParser = require('body-parser');
 var router = express.Router(); 
 var port = process.env.PORT || 1337;
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 router.get('/', function(req, res) {
     res.json({ message: 'Server is up and running' });   
 });
