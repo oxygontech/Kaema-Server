2e2e67abe0a613040f9f2bbd6997dd084108e4b6
Jithendra <jithendrathenu@gmail.com>
2018-03-20 22:06:31 +0530

    Scoring Module Added and minor changes in Server configure to support JSON

--- firebase-modules/profile-module.js
+++ firebase-modules/profile-module.js
@@ -1,18 +1,18 @@
-var admin = require("firebase-admin");
+/*var admin = require("firebase-admin");
 
 var serviceAccount = require(".././kaema-159c6-firebase-adminsdk-phdnc-9a139dbaf7.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://kaema-159c6.firebaseio.com"
-});
+});*/
 
 
 
 
 
 
-exports.profile_changed_listener=function (){
+exports.profile_changed_listener=function (admin){
 
     var db = admin.database();
     var ref = db.ref("/profile");
