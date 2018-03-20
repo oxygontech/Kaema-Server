cb3e51bd8bb594b9131f8d7f1922dbbf0262199b
Jithendra <jithendrathenu@gmail.com>
2018-03-20 22:55:12 +0530

    Allowed Post

--- Server.js
+++ Server.js
@@ -38,6 +38,7 @@ app.use(function (req, res, next) {
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
