8934547c82ade44636760c7f2584015abeabfa1a
Jithendra <jithendrathenu@gmail.com>
2018-03-20 23:01:44 +0530

    Origin changed

--- Server.js
+++ Server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json())
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
