47644c895407ed64fcee88f6f61b26575c98e218
hasoxy <hasoxy@gmail.com>
2018-04-01 16:34:49 +0530

    Modifications to server and wastemonitor module

--- Server.js
+++ Server.js
@@ -58,11 +58,10 @@ router.post('/profile', function(req, res) {
 
 //this will send the weight of the bin to the firebase.
 app.post('/binweight', function(req, res) {
-  console.log("Kaema");
 	console.log(JSON.stringify(req.body));
   firebaseWasteMonitor.post_reading(admin,req.body);
 	//console.log(req);
- res.json({ "message": 'Weight Recieved' });
+  res.json({ "message": 'Weight Recieved' });
 });
 //app.use('/service', router);
 //listen to shares
--- firebase-modules/firebase-wastemonitor.js
+++ firebase-modules/firebase-wastemonitor.js
@@ -4,22 +4,9 @@ var db = admin.database();
 //assinging the name of the db ref to a variable
 var leader_boardRef = db.ref("current_reading/");
 leader_boardRef.child(readingObj.binId).set({
-          weight:parseInt(readingObj.weight)
-          //userId:shared.post.userId,
-        //  userProfile:shared.post.userProfile
-        });
-
-}
-
-
-exports.post_reading = function (admin,readingObj){
-console.log(readingObj);
-var db = admin.database();
-//assinging the name of the db ref to a variable
-var leader_boardRef = db.ref("current_reading/");
-leader_boardRef.child(readingObj.binId).set({
-          weight:parseInt(readingObj.weight)
-          //userId:shared.post.userId,
+          weight:parseInt(readingObj.weight),
+          height:parseInt(readingObj.height),
+          door:parseInt(readingObj.door)
         //  userProfile:shared.post.userProfile
         });
 
